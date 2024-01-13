3d4b1db4ab02bf6b7cd89f7eb6ffad2d7ae9bc70
LeBigCronch <100243872+LeBigCronch@users.noreply.github.com>
2024-01-13 18:13:39 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+sudo dnf install tmux htop vim gcc java-17-openjdk-devel.x86_64 firefox libreoffice nvim kitty fish python git tlp powertop
+mkdir git_stuff
+cd git_stuff
+git clone https://github.com/pystardust/ani-cli
+git clone https://github.com/sleepntsheep/susfetch
+cd ..
